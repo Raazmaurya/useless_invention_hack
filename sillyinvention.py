58a269a956b1854cc413dabffab7a80b41b9a885
Raaz <80684783+Raazmaurya@users.noreply.github.com>
2021-06-30 17:44:04 +0530

    Add files via upload

---
+++ sillyinvention.py
@@ -0,0 +1,5 @@
+x = int(input("Enter 1 if you are a human : "))
+if x==1:
+    print("Siddharth is a very Cute boy !! You must have a crush on him.")
+else:
+    print("You are an idiot, Run again and Enter 1 !!")
